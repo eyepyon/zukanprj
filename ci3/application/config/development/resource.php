d111466ae9e9c754b13d2701526547f5d67c4e22
eyepyon <eyepyon@gmail.com>
2021-01-19 03:45:02 +0900

    path

--- ci3/application/config/development/resource.php
+++ ci3/application/config/development/resource.php
@@ -6,7 +6,7 @@
  * @version: 2019-03-16 14:33
  */
 //$config['base_url'] = 'http://dev.zukan.cloud/';
-$config['base_url'] = 'https://www.zukan.cloud/';
+$config['base_url'] = 'https://dev.zukan.cloud/';
 // アップ用のディレクトリ
 //$config['file_upload_dir'] = '/home/sites/www.zukan.cloud/wwwroot/zukanprj/html/files/';
 $config['file_upload_dir'] = '/var/www/zukanprj/web/files/';
@@ -14,7 +14,7 @@ $config['file_upload_dir'] = '/var/www/zukanprj/web/files/';
 $config['api_type_add_id'] = 1;
 
 $config['app_sv_maintenance_mode'] = 0;// 0,OFF 1,ON
-$config['app_sv_maintenance_redirect_url'] = 'http://zukan.cloud/mente.php';// メンテページURL
+$config['app_sv_maintenance_redirect_url'] = 'http://www.zukan.cloud/mente.php';// メンテページURL
 
 
 // メンテ対象外IP
