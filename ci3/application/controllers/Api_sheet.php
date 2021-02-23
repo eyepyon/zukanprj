6303e87d67358ad498edf77dd6d1ca0392f72a79
eyepyon <eyepyon@gmail.com>
2021-02-23 11:49:08 +0900

    デザイン更新

--- ci3/application/controllers/Api_sheet.php
+++ ci3/application/controllers/Api_sheet.php
@@ -162,15 +162,21 @@ class Api_sheet extends CI_Controller
 						// 最新のヒストリーから更新日時を取得
 						$lasts = $this->recordModel->getLastUpdate($record["id"]);
 						print_r($lasts);
-						if (isset($lasts['form_timestamp']) && isset($return['form_timestamp'])
+						if(is_null($lasts)){
+							// 更新されているのでデータ更新
+							$this->recordModel->setRecordData($record['id'],$return);
+							// ヒストリー
+							$this->recordModel->setImportHistory($record["id"],$return['form_timestamp']);
+							print("更新1");
+						}elseif (isset($lasts['form_timestamp']) && isset($return['form_timestamp'])
 							&& (strtotime($return['form_timestamp']) > strtotime($lasts ['form_timestamp']))) {
 							// 更新されているのでデータ更新
 							$this->recordModel->setRecordData($record['id'],$return);
 							// ヒストリー
 							$this->recordModel->setImportHistory($record["id"],$return['form_timestamp']);
 							print("更新");
-
 						}
+
 					} else {
 						// データ無いのでインサート
 						$record_id = $this->recordModel->setRecordData(0,$return);
